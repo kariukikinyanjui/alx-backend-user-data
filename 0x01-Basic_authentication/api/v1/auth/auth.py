87a3c05c618299c7f597693ea6c541ad263be816
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-04-17 20:30:05 +0000

    [feat]: defined which routes don't need authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,17 @@ class Auth:
     '''Class for managing API authentication'''
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         '''Check if the authentication is required for the given path.'''
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or not excluded_paths:
+            return True
+
+        path = path.rstrip('/') + '/'
+
+        for excluded_path in excluded_paths:
+            if path.startswith(excluded_path):
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         '''Get the authorization header from the request'''
